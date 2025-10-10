6ae9f554622896bf1d476ec30794ff7fe224de5e
sv3048 <sv3048@gmail.com>
2025-10-10 21:47:50 +0200

    update weight for bveto_booSF

--- Analysis.py
+++ Analysis.py
@@ -94,7 +94,9 @@ def makeRDF(dataset_name):
     df = df.Define("CleanJet_notOverlapping", "getCleanJetNotOverlapping(FatJet_eta[GoodFatJet_idx], FatJet_phi[GoodFatJet_idx], CleanJet_eta, CleanJet_phi)")
     df = df.Define("bVeto_boo", "bVeto_boo(CleanJet_pt, CleanJet_eta, CleanJet_jetIdx, Jet_btagDeepFlavB, CleanJet_notOverlapping)")
     df = df.Filter("bVeto_boo", "bjet veto")
-    df = df.Define("bVeto_booSF","bVeto_booSF(CleanJet_pt, CleanJet_eta, CleanJet_jetIdx, Jet_btagSF_deepjet_shape, CleanJet_notOverlapping, 20.0)")
+    if isMC:
+        df = df.Define("bVeto_booSF","bVeto_booSF(CleanJet_pt, CleanJet_eta, CleanJet_jetIdx, Jet_btagSF_deepjet_shape, CleanJet_notOverlapping, 20.0)")
+        df = df.Redefine("weight","weight*bVeto_booSF")
 
     results["Cutflow5"] = df.Histo1D(("h_cutflow_5","Cutflow 5",1,-0.5,0.5),"cutflow_stage","weight")
 
